5697aa8e0c780aa58601cf6bf362dca3b492c666
alasinofranco3 <falasino@fi.uba.ar>
2020-05-13 02:11:35 -0300

    primeras correciones

--- common_resizable_buffer.c
+++ common_resizable_buffer.c
@@ -39,22 +39,6 @@ int resizable_buffer_resize(resizable_buffer_t *self, int new_size){
 
 int resizable_buffer_save(resizable_buffer_t *self, char *word){
 	int status;
-	/*int new_size = strlen(self->buffer) + strlen(word);
-	void *ptr_status;
-
-	if (new_size >= self->size - 1) {
-		status = resizable_buffer_resize(self, new_size + 1);
-		if (status == ERROR) return ERROR;
-	}
-
-	ptr_status = strncat(self->buffer, word, strlen(word));
-	if (ptr_status == NULL) {
-		resizable_buffer_destroy(self);
-		return ERROR;
-	} 
-
-	return 0;
-	*/
 	status = resizable_buffer_n_save(self, word, strlen(word));
 	return status;
 }
@@ -63,12 +47,6 @@ int resizable_buffer_n_save(resizable_buffer_t *self, char *word, int n){
 	int new_size = strlen(self->buffer) + n + 1;
 	int status;
 	void *ptr_status;
-
-	/*
-	if (new_size > self->size) {
-		status = resizable_buffer_resize(self, new_size);
-		if (status == ERROR) return ERROR;
-	}*/
 
 	status = resize_if_needed(self, new_size);
 	if (status == ERROR) return ERROR;
@@ -87,20 +65,8 @@ int resizable_buffer_byte_save(resizable_buffer_t *self, char* buf, int size) {
 	int old_size = self->size;
 	int	new_size = self->size + size;
 
-	/*
-	if (new_size > self->size) {
-		status = resizable_buffer_resize(self, new_size);
-		if (status == ERROR) return ERROR;
-	}
-	*/
-
 	status = resize_if_needed(self, new_size);
 	if (status == ERROR) return ERROR;
-
-	/*
-	for (int i = 0 ; i < size; i++) {
-		self->buffer[i + old_size] = buf[i];
-	}*/
 
 	memcpy(&self->buffer[old_size], buf, size);
 
